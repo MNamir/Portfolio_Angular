3316442a97cbb348d73d6f9b8c65e63d54b57786
MohdNameer <mohd.nameer@quovantis.com>
2019-11-04 15:15:59 +0530

    implement if and for loop on about page

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+  oddNumber = [1, 3, 5, 7, 9];
+  evenNumber = [2, 4, 6, 8, 10];
+  onlyOdd = false;
   constructor() { }
 
   ngOnInit() {
